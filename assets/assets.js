33b83abed246e12a3a4e9bc8c1dd6e2b0ba3d2b0
pablo-cunha <lcunha.pablo@gmail.com>
2025-01-21 20:09:07 -0300

    Atualiza Assets

---
+++ assets/assets.js
@@ -0,0 +1,89 @@
+import user_image from './gthub-profile.jfif';
+import code_icon from './code-icon.png';
+import code_icon_dark from './code-icon-dark.png';
+import edu_icon from './edu-icon.png';
+import edu_icon_dark from './edu-icon-dark.png';
+import project_icon from './project-icon.png';
+import project_icon_dark from './project-icon-dark.png';
+import right_arrow_white from './right-arrow-white.png';
+import mail_icon from './mail_icon.png';
+import mail_icon_dark from './mail_icon_dark.png';
+import download_icon from './download-icon.png';
+import hand_icon from './hand-icon.png';
+import header_bg_color from './header-bg-color.png';
+import moon_icon from './moon_icon.png';
+import sun_icon from './sun_icon.png';
+import arrow_icon from './arrow-icon.png';
+import arrow_icon_dark from './arrow-icon-dark.png';
+import menu_black from './menu-black.png';
+import menu_white from './menu-white.png';
+import close_black from './close-black.png';
+import close_white from './close-white.png';
+import right_arrow from './right-arrow.png';
+import send_icon from './send-icon.png';
+import right_arrow_bold from './right-arrow-bold.png';
+import right_arrow_bold_dark from './right-arrow-bold-dark.png';
+
+export const assets = {
+    user_image,
+    code_icon,
+    code_icon_dark,
+    edu_icon,
+    edu_icon_dark,
+    project_icon,
+    project_icon_dark,
+    right_arrow_white,
+    mail_icon,
+    mail_icon_dark,
+    download_icon,
+    hand_icon,
+    header_bg_color,
+    moon_icon,
+    sun_icon,
+    arrow_icon,
+    arrow_icon_dark,
+    menu_black,
+    menu_white,
+    close_black,
+    close_white,
+    right_arrow,
+    send_icon,
+    right_arrow_bold,
+    right_arrow_bold_dark
+};
+
+export const workData = [
+    {
+        title: 'Site de Bicicletas Elétricas',
+        description: 'Aplicação Web',
+        bgImage: './public/bikcraft.JPG',
+    },
+    {
+        title: 'Buscador de CEP',
+        description: 'Aplicação Web',
+        bgImage: './public/buscador-cep.JPG',
+    },
+    {
+        title: 'Github Search',
+        description: 'Aplicação Web',
+        bgImage: './public/git-search.JPG',
+    },
+    {
+        title: 'Lista de Taredas',
+        description: 'Aplicação Web',
+        bgImage: './public/vue-todolist.jpg',
+    },
+]
+
+// export const serviceData = [
+//     { icon: assets.web_icon, title: 'Desenvolvimento Web', description: 'Desenvolvedor Front-end com experiências em NextJS, ReactJS e VueJS', link: '' },
+//     { icon: assets.mobile_icon, title: 'Segurança', description: 'Conceitos e boas práticas com criptografia e desenvolvimento seguro', link: '' },
+//     { icon: assets.ui_icon, title: 'Banco de Dados', description: 'SQL, MariaDB e PostgresSQL', link: '' },
+//     { icon: assets.graphics_icon, title: 'Análise de Dados', description: 'Experiente no tratamento de dados e no desenvolvimento de dashboards em Python', link: '' },
+// ]
+
+export const infoList = [
+    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Linguagens de Programação', description: 'HTML, CSS, JavaScript, PHP e Python' },
+    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Educação', description: 'Tecnólogo em Análise e Desenvolvimento de Sistemas' },
+    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Frameworks', description: 'ReactJS, NextJS e VueJS' }
+];
